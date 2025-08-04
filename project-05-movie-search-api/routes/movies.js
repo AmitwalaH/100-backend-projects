81cb82fcb01f635acea5a03a26d75cbcda11e7ba
AmitwalaH <algoworks818@gmail.com>
2025-08-04 18:03:43 +0530

    done project-05-movie-search-api

--- project-05-movie-search-api/routes/movies.js
+++ project-05-movie-search-api/routes/movies.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 
 dotenv.config();
 
-const TMDB_API_KEY = process.env.TMDB_API_KEY;
+const TMDB_API_KEY = process.env.TMDB_API_KEY; // Insert your TMDB API key here
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 
 router.get("/search", async (req, res) => {
