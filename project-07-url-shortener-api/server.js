29f36b584acd2e7617b1ec7656f6946703bf7d73
AmitwalaH <algoworks818@gmail.com>
2025-08-07 21:51:05 +0530

    done project-07-url-shortener-api

--- project-07-url-shortener-api/server.js
+++ project-07-url-shortener-api/server.js
@@ -11,11 +11,11 @@ dotenv.config();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGO_URI = process.env.MONGODB_URI;
 
 // Connect to MongoDB
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
