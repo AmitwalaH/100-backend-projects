e0d046041e0db602e740e4c07a46746d970c2d98
AmitwalaH <algoworks818@gmail.com>
2025-08-31 15:01:14 +0530

    feat: Add logging middleware with Winston and Morgan

---
+++ project-18-advanced-api/middleware/logger.js
@@ -0,0 +1,30 @@
+const winston = require('winston');
+const morgan = require('morgan');
+
+const logFormat = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.json()
+);
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: logFormat,
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+const morganMiddleware = morgan(
+  ':method :url :status :res[content-length] - :response-time ms',
+  {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  }
+);
+
+module.exports = {
+  logger,
+  morganMiddleware,
+};
