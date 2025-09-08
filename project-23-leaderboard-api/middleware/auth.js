8d555bb4e14dcafb2db8c35667291d6b4c78add9
AmitwalaH <algoworks818@gmail.com>
2025-09-08 15:58:46 +0530

    chore: Add auth middleware and API routes for user

---
+++ project-23-leaderboard-api/middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// Middleware to protect routes by verifying JWTs
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Extract the token from the header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify the token using the secret key
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      // Handle errors if the token is invalid or expired
+      console.error("JWT verification failed:", error);
+      res.status(401).json({ error: "Not authorized, token failed." });
+    }
+  }
+
+  // If no token is provided in the request headers
+  if (!token) {
+    res.status(401).json({ error: "Not authorized, no token." });
+  }
+};
+
+module.exports = { protect };
