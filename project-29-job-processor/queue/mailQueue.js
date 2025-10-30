f21bf0090cd60a5b0a7efa61918e755da54f940c
AmitwalaH <algoworks818@gmail.com>
2025-10-30 23:09:44 +0530

    feat: Created BullMQ queue definition

---
+++ project-29-job-processor/queue/mailQueue.js
@@ -0,0 +1,20 @@
+import Queue from "bullmq";
+
+const connection = {
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || 6379,
+};
+
+// Initialize the Queue instance
+const mailQueue = new Queue("email-queue", { connection });
+
+async function addMailJob(email, subject, body) {
+  // Add the job to the Redis queue
+  await mailQueue.add("sendEmail", { email, subject, body });
+  console.log(`Job added to queue for email: ${email}`);
+}
+
+module.exports = {
+  addMailJob,
+  connection,
+};
