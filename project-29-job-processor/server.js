336d2b2b00851084eb88cfb3b3547abefdbcc97d
AmitwalaH <algoworks818@gmail.com>
2025-10-30 23:27:23 +0530

    feat: Complete job processor server setup

---
+++ project-29-job-processor/server.js
@@ -0,0 +1,17 @@
+const { express } = require("express");
+const dotenv = require("dotenv");
+require("./queue/worker"); // Start the worker
+const jobsRouter = require("./routes/jobs");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("api/jobs", jobsRouter);
+
+// Starting the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
