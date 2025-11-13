9a1a1e4edd94fb1745262ea9e97ba0e6948a20ce
AmitwalaH <algoworks818@gmail.com>
2025-11-13 21:37:52 +0530

    feat: Add API route for authors

---
+++ project-33-book-author-api/routes/authors.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/Author");
+
+// POST /api/post
+router.post("/api/post", async (req, res) => {
+  try {
+    const author = req.body;
+    author = new Author();
+    await author.save();
+    res.status(201).json(author);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//GET /api/get
+router.get("/api/get", async (req, res) => {
+  try {
+    const author = await Author.find();
+    res.json(author);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
