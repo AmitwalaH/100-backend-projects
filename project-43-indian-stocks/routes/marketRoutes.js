d501e1532f6c70ed77c8de4f11de15a1e3d8e31f
AmitwalaH <algoworks818@gmail.com>
2026-01-07 14:36:56 +0530

    feat: Add route to get the name of ticker

---
+++ project-43-indian-stocks/routes/marketRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const { fetchStockData } = require("../services/marketService");
+
+// GET /api/stocks/:name
+router.get("/:name", async (req, res) => {
+  try {
+    const data = await fetchStockData(req.params.name);
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
