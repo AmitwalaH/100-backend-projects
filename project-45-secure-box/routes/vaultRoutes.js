78b174cd648ffd727b3c77d9c190fbbd2fea895c
AmitwalaH <algoworks818@gmail.com>
2026-01-11 17:00:07 +0530

    feat: Add vault API route for secret

---
+++ project-45-secure-box/routes/vaultRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const { saveSecret, getSecret } = require("../services/vaultController");
+
+router.post("/add", (req, res) => {
+  try {
+    const { title, secret } = req.body;
+
+    saveSecret(title, secret);
+
+    res.status(200).json({ message: "Secret saved successfully" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to save secret", details: err.message });
+  }
+});
+
+router.get("/:title", (req, res) => {
+  try {
+    const { title } = req.params;
+
+    const secret = getSecret(title);
+
+    if (!secret) {
+      return res.status(404).json({ error: "Secret not found" });
+    }
+
+    res.status(200).json({ title, secret });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve secret", details: err.message });
+  }
+});
+
+module.exports = router;
