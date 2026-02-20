7baad20c1790005727035df0a57a9f725d0dee07
AmitwalaH <algoworks818@gmail.com>
2026-02-20 17:27:03 +0530

    Fix nexus frontend push notification error

--- project-48-nexus/server.js
+++ project-48-nexus/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-const nexusManager = require("./nexusManager");
+const { initializeNexus } = require("./managers/nexusManager");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,21 +10,13 @@ app.use(cors());
 
 const io = new Server(server, {
   cors: {
-    origin: "*",   
+    origin: "*",
   },
   pingInterval: 25000,
   pingTimeout: 60000,
 });
 
-io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
-
-  nexusManager.handleConnection(io, socket);
-
-  socket.on("disconnect", (reason) => {
-    console.log(`User disconnected: ${reason}`);
-  });
-});
+initializeNexus(io);
 
 server.listen(3000, () => {
   console.log("Server running on port 3000");
